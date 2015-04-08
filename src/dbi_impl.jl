998a5ef6616668020eaa076a014501fe652d2262
Clemente <cuevasclemente@gmail.com>
2015-04-08 12:52:11 -0400

    Added note about do conn notation

--- src/dbi_impl.jl
+++ src/dbi_impl.jl
@@ -27,6 +27,8 @@ function Base.connect(::Type{Postgres},
     Base.connect(Postgres, host, user, passwd, db, string(port))
 end
 
+# Note that for some reason, `do conn` notation
+# doesn't work using this version of the function
 function Base.connect(::Type{Postgres};
                       dsn::String="")
     conn = PQconnectdb(dsn)
